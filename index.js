b497bf503b9608ecc22af7c9804c378e57a1ddec
WellingtonVinii <wellingtonvinii10@gmail.com>
2024-11-24 02:01:30 -0300

    teste

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ function cadastrarClienteView(req, res) {
         <head>
             <meta charset="UTF-8">
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <title>Cadastro de Cliente</title>
+            <title>Cadastro de produto</title>
             <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous">
             <style>
                 .error-message {
@@ -321,3 +321,5 @@ app.post('/cadastrarCliente', verificarAutenticacao, cadastrarCliente);
 app.listen(porta, host, () => {
     console.log('Servidor iniciado e em execução no endereço http://localhost:3000');
 })
+
+export default app;
